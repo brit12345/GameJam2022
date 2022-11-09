dbd0bd5d3813484a0003962a9228842ba92dcba1
Bella <isabella.vaneyk@gmail.com>
2022-11-09 21:30:17 +1000

    ahhhhhh spawning sucks

--- js/enemy.js
+++ js/enemy.js
@@ -1,76 +1,76 @@
 
 class Enemy {
   
-    constructor(){
-      this.sprite;
-      this.alive = true;
-      this.walkSpeed = 1.5;
-      this.forwardAngle = 180
-      this.shootSpeed = 7;
-      this.cooldownTimer = 2000;
-      this.cooldownDuration = 600;
-      this.spawnTimer = 1000
-      this.spawnDuration = 1000
-      
-    }
-  
-    preload(){
-  
-    }
-  
-    setup(){
-      
-      this.sprite = createSprite(500, height-80, 40, 40);
-      this.sprite.health = 30; //health needs to be attached to sprite for access in takeDamage function
-      this.sprite.setCollider("rectangle",0 ,0, 40,40)
-      this.sprite.takeDamage = function (amt){
-        this.health -= amt; //in here, "this" refers to the sprite, not the object
-      }
-      this.sprite.debug=true;
-      gameSprites.add(this.sprite);
-      enemies.add(this.sprite);
-    }
-  
-    draw(){
-      drawSprites();
-      this.movement();
-      this.checkForShoot();
-     
-      if(this.sprite.health <= 0){
-        this.die();
-      }
-    }
-  
-   
-    movement(){
-      if (player.sprite.position.x >= this.sprite.position.x) {
-          
-          this.sprite.position.x += this.walkSpeed
-          this.forwardAngle = 0;
-      } else if (player.sprite.position.x <= this.sprite.position.x) {
-   
-        this.sprite.position.x -= this.walkSpeed
-        this.forwardAngle = 180;
-  
-      }
-    }
+  constructor(){
+    this.sprite;
+    this.alive = true;
+    this.walkSpeed = 1.5;
+    this.forwardAngle = 180
+    this.shootSpeed = 7;
+    this.cooldownTimer = 2000;
+    this.cooldownDuration = 600;
+    this.spawnTimer = 1000
+    this.spawnDuration = 1000
+    
+  }
 
+  preload(){
 
-    checkForShoot(){
-      this.cooldownTimer++;
-      if(this.cooldownTimer >= this.cooldownDuration/this.shootSpeed && (this.alive)){
-        this.shoot();
-        this.cooldownTimer = 0;
-      }
+  }
+
+  setup(){
+    
+    this.sprite = createSprite(500, height-80, 40, 40);
+    this.sprite.health = 30; //health needs to be attached to sprite for access in takeDamage function
+    this.sprite.setCollider("rectangle",0 ,0, 40,40)
+    this.sprite.takeDamage = function (amt){
+      this.health -= amt; //in here, "this" refers to the sprite, not the object
     }
-  
-    shoot(){
-      let Enemybullet = new EnemyBullet(this.sprite.position.x, this.sprite.position.y);
-      Enemybullet.shoot(this.forwardAngle, this.shootSpeed); //shoot forward
-    }
-  
-    die(){
-      this.sprite.remove();
-      this.alive = false;
+    this.sprite.debug=true;
+    gameSprites.add(this.sprite);
+    enemies.add(this.sprite);
+  }
+
+  draw(){
+    drawSprites();
+    this.movement();
+    this.checkForShoot();
+    
+    if(this.sprite.health <= 0){
+      this.die();
     }
   }
+
+  
+  movement(){
+    if (player.sprite.position.x >= this.sprite.position.x) {
+        
+        this.sprite.position.x += this.walkSpeed
+        this.forwardAngle = 0;
+    } else if (player.sprite.position.x <= this.sprite.position.x) {
+  
+      this.sprite.position.x -= this.walkSpeed
+      this.forwardAngle = 180;
+
+    }
+  }
+
+
+  checkForShoot(){
+    this.cooldownTimer++;
+    if(this.cooldownTimer >= this.cooldownDuration/this.shootSpeed && (this.alive)){
+      this.shoot();
+      this.cooldownTimer = 0;
+    }
+  }
+
+  shoot(){
+    let Enemybullet = new EnemyBullet(this.sprite.position.x, this.sprite.position.y);
+    Enemybullet.shoot(this.forwardAngle, this.shootSpeed); //shoot forward
+  }
+
+  die(){
+    this.sprite.remove();
+    this.alive = false;
+  }
+}
--- js/enemySpawn.js
+++ js/enemySpawn.js
@@ -1,24 +1,24 @@
 
 class EnemySpawn {
 
-    constructor(){
-      this.sprite;
-      this.sprite = createSprite(500, 500, 30, 30);
-      this.sprite.setCollider("rectangle", 0, 0, 30, 30);
-      gameSprites.add(this.sprite);
-      enemies.add(this.sprite);
-    }
-
-    checkForSpawn(){
-        enemy.spawnTimer++;
-        if(enemy.spawnTimer >= enemy.spawnDuration){
-          this.spawn();
-          enemy.spawnTimer = 0;
-        }
-      }
-    
-      spawn(){
-        let enemySpawn = new Enemy(this.sprite.position.x, this.sprite.position.y);
-      
-    }
+  constructor(){
+    this.sprite;
+    this.sprite = createSprite(500, 500, 30, 30);
+    this.sprite.setCollider("rectangle", 0, 0, 30, 30);
+    gameSprites.add(this.sprite);
+    enemies.add(this.sprite);
   }
+
+  checkForSpawn(){
+      enemy.spawnTimer++;
+      if(enemy.spawnTimer >= enemy.spawnDuration){
+        this.spawn();
+        enemy.spawnTimer = 0;
+      }
+    }
+  
+    spawn(){
+      let enemySpawn = new Enemy(this.sprite.position.x, this.sprite.position.y);
+    
+  }
+}
